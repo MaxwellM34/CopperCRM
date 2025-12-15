fe60951ec8fa8c87ed798f20221d9a6c74cb5a64
Maxwell McInnis <maxwell@ecoli-sense.com>
2025-12-15 09:37:35 -0500

    added cors middleware

--- api/app.py
+++ api/app.py
@@ -39,9 +39,8 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "http://localhost:3000",
-            "http://localhost:5173",
-            "http://localhost:1234",
+            "http://localhost:8000",
+            "https://crm-api-468831678336.us-central1.run.app"
             # Add your Cloud Run URL later (once created)
         ],
         allow_credentials=True,
