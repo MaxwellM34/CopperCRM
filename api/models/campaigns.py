df3f3923bbe8b1faa134112560fddfc5ce5a32e1
MaxwellM34 <maxwellmcinnis123@gmail.com>
2026-01-05 17:25:31 -0600

    planning campaigns

---
+++ api/models/campaigns.py
@@ -0,0 +1,15 @@
+'''
+
+This file will be holding the information about what the heck is going on with a lead
+that we want to email in a cold outbound campaign or define whether we have spoken to
+them already and should not act like we haven't taked to them before. Items we will 
+need to add:
+
+- There needs to be a thing that marks whether a lead has been contact before or not
+- need connections to other models that contain IMAP and stuff that includes peoples
+  inboxes or maybe conversations, also their stage.
+- add more... running out of charge
+- 
+
+
+'''
