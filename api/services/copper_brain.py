cafeab9658d15963ef8165cd577cbea3ec50e54e
MaxwellM34 <maxwellmcinnis123@gmail.com>
2026-01-05 16:19:28 -0600

    test

--- api/services/copper_brain.py
+++ api/services/copper_brain.py
@@ -1,2 +1,4 @@
 '''This is where the brain of Copper will exist it will take all of the information in the models / db
-and decide what the heck to do with leads'''
+and decide what the heck to do with leads
+
+This is mainly to control marketting campaigns not anything else'''
